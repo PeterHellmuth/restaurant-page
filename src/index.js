7b940cc4a30e84560a0ec69dca9e1a05ee010cb2
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-09-25 15:40:19 -0500

    Add CSS and image bundling with webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("test");
+import './style.css';
+import backgroundImage from './background.jpg';
+
+
+const homeBackground = new Image();
+homeBackground.src = backgroundImage;
+homeBackground.classList.add("background-image");
+
+const contentElem = document.getElementById("content");
+contentElem.appendChild(homeBackground);
